d45ceb7e1f7e345548eba6f36da329c8a95f42e7
Manea DL <96151145+Maya7dit@users.noreply.github.com>
2022-04-06 21:34:14 +0300

    Add files via upload

---
+++ Functii.py
@@ -0,0 +1,11 @@
+# functie -> o logica delimitata care poate fi refolosita
+#adica trb sa stim de udne pana unde tine logica , un inceput si sf
+#avantajul-> poate sa fie refolosita
+
+# functie simpla exemplu : o functie simpla care ne printeaza ceva pe ecran\
+            # nu are return( nu ne da raspuns
+            # nu are paramentri
+
+# reguli la functii:
+#     // * Fara spatii in nume
+#     // *  nu putem defini a functie in alta funcite( ex nu pot instala firefox in chrome)
